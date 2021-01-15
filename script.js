24ef6aa47723c82ffed374ad4a7dc1ea1ef836f6
Paul Ladd <github@itspladd.com>
2021-01-15 11:37:08 -0600

    Added getAbsoluteDistance function

--- script.js
+++ script.js
@@ -7,11 +7,15 @@ let currentRoundNumber = 1;
 //generateTarget: Returns a random number between 0 and 9.
 const generateTarget = () => Math.floor(Math.random() * 10);
 
+//compareGuesses: Takes in three numbers and determines which of the first two numbers is closest to the third.
+//		  Returns TRUE if the first number is closest, and FALSE if the second number is closest.
 function compareGuesses(humanGuess, computerGuess, targetNumber) {
 	let humanDistance = humanGuess > targetNumber ? (humanGuess - targetNumber) : (targetNumber - humanGuess)
 	let computerDistance = computerGuess > targetNumber ? (computerGuess - targetNumber) : (targetNumber - computerGuess);
 	return humanDistance <= computerDistance ? true : false;
 	}
+
+const getAbsoluteDistance = (num1, num2) => Math.abs(num1-num2);
 
 function updateScore(winner) {
 	if (winner === 'human')
