e09fc93cf9c7753f86154a2f2885570ad3234587
Paul Ladd <github@itspladd.com>
2021-01-15 11:58:04 -0600

    Added alert to notify user if they input a number outside 0-9

--- script.js
+++ script.js
@@ -10,10 +10,14 @@ const generateTarget = () => Math.floor(Math.random() * 10);
 //compareGuesses: Takes in three numbers and determines which of the first two numbers is closest to the third.
 //		  Returns TRUE if the first number is closest, and FALSE if the second number is closest.
 function compareGuesses(humanGuess, computerGuess, targetNumber) {
+	if (humanGuess < 0 || humanGuess > 9) {
+		alert(`Your number must be between 0 and 9! Your number: ${humanGuess}`);
+		return false;
+	}
 	let humanDistance = getAbsoluteDistance(humanGuess, targetNumber);
 	let computerDistance = getAbsoluteDistance(computerGuess, targetNumber);
 	//console.log(`Human distance: ${humanDistance}, computer distance: ${computerDistance}`);
-	return humanDistance <= computerDistance ? true : false;
+	return humanDistance <= computerDistance;
 	}
 
 const getAbsoluteDistance = (num1, num2) => Math.abs(num1-num2);
